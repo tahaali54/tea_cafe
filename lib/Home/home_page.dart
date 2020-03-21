b4fefb48277a2a12ae716287288ab54ad1eee951
Taha Ali <tahaali54@gmail.com>
2020-03-21 13:09:20 +0500

    add popular items list on home page

--- lib/Home/home_page.dart
+++ lib/Home/home_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:tea_cafe/CustomWidgets/custom_scaffold.dart';
 
+import 'popular_items.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
       index: 0,
       title: 'Tea & Cafe',
-      body: Container(),
+      body: PopularItems(),
     );
   }
 }
---
+++ lib/Home/popular_items.dart
@@ -0,0 +1,149 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:tea_cafe/CustomWidgets/circular_button.dart';
+import 'package:tea_cafe/CustomWidgets/custom_scaffold.dart';
+
+import '../global.dart';
+
+class PopularItems extends StatelessWidget {
+  final List<Map> items = [
+    {"img": "assets/food1.jpeg", "name": "Fruit Salad"},
+    {"img": "assets/food2.jpeg", "name": "Fruit Salad"},
+    {"img": "assets/food3.jpeg", "name": "Hamburger"},
+    {"img": "assets/food4.jpeg", "name": "Fruit Salad"},
+    {"img": "assets/food5.jpeg", "name": "Hamburger"},
+    {"img": "assets/food6.jpeg", "name": "Steak"},
+    {"img": "assets/food7.jpeg", "name": "Pizza"},
+    {"img": "assets/food8.jpeg", "name": "Asparagus"},
+    {"img": "assets/food9.jpeg", "name": "Salad"},
+    {"img": "assets/food10.jpeg", "name": "Pizza"},
+    {"img": "assets/food11.jpeg", "name": "Pizza"},
+    {"img": "assets/food12.jpg", "name": "Salad"},
+  ];
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> gridItems = List();
+
+    return ListView(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(
+                'Popular Items',
+                style: setTextStyle(
+                    size: 23, color: primaryTextColor, weight: FontWeight.w800),
+              ),
+            ),
+            FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'View More',
+                  style: setTextStyle(
+                      color: primaryColor, weight: FontWeight.bold, size: 16),
+                ))
+          ],
+        ),
+        StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection('popular_items').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return LinearProgressIndicator();
+
+            return _buildGridView(context, snapshot.data.documents, gridItems);
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildGridView(
+      BuildContext context, List<DocumentSnapshot> documents, List<Widget> x) {
+    List<Widget> getRatingRow() {
+      List<Widget> row = List();
+      for (int i = 0; i < 5; i++) {
+        row.add(Icon(
+          Icons.star,
+          color: Colors.orangeAccent,
+          size: 11,
+        ));
+      }
+      row.add(Container(
+        width: 8,
+      ));
+      row.add(
+        Text(
+          '5.0 (23 Reviews)',
+          style: setTextStyle(size: 11, color: primaryTextColor),
+        ),
+      );
+      return row;
+    }
+
+    int counter = 0;
+
+    documents.forEach((item) {
+      x.add(
+        InkWell(
+            onTap: () {},
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset(
+                        items[counter]['img'],
+                        height: MediaQuery.of(context).size.height / 3.6,
+                        width: MediaQuery.of(context).size.width / 2.2,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      height: 30,
+                      width: 30,
+                      right: 8.0,
+                      bottom: 8.0,
+                      child: CircularButton(
+                          icon: Icon(
+                            Icons.favorite_border,
+                            color: primaryColor,
+                            size: 15,
+                          ),
+                          onPressed: () {}),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 2),
+                  child: Text(
+                    item['name'],
+                    style: setTextStyle(
+                        size: 20,
+                        weight: FontWeight.w900,
+                        color: primaryTextColor),
+                  ),
+                ),
+                Row(
+                  children: getRatingRow(),
+                ),
+              ],
+            )),
+      );
+      counter++;
+    });
+    return GridView.count(
+      shrinkWrap: true,
+      primary: false,
+      padding: const EdgeInsets.all(8),
+      crossAxisSpacing: 8,
+      childAspectRatio: MediaQuery.of(context).size.width /
+          (MediaQuery.of(context).size.height / 1.25),
+      mainAxisSpacing: 8,
+      crossAxisCount: 2,
+      children: x,
+    );
+  }
+}
